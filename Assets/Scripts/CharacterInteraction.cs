d9699e5d2485a8f2ec9dd2ef0d569df58821baad
Danylo Hukov <48653308+DHukov@users.noreply.github.com>
2023-02-04 12:15:02 +0100

    Simple dialogs
    Added mechanic for dialog

---
+++ Assets/Scripts/CharacterInteraction.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using TMPro;
+
+public class CharacterInteraction : MonoBehaviour
+{
+    private bool isInRange = false;
+    [SerializeField] private TMP_Text textComponent;
+    public KeyCode interactKey;
+    [SerializeField] private Canvas canvasActive;
+    public string[] dialogText;
+    private int index;
+    void Start()
+    {
+        canvasActive.gameObject.active = false;
+    }
+    private void Update()
+    {
+        InteractAction();
+    }
+    private void InteractAction()
+    {
+        if (isInRange) 
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                if (index <= dialogText.Length - 1)
+                {
+                    textComponent.text = dialogText[index];
+                    index++;
+                    //throw new System.Exception("Invalid index");
+                }
+            }
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Check interaction Enter");
+            isInRange = true;
+            canvasActive.gameObject.active = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Check interaction Exit");
+            canvasActive.gameObject.active = false;
+
+            isInRange = false;
+        }
+    }
+}
