410df0a468ca17d5da68539e3bdee81c749715c2
Danylo Hukov <48653308+DHukov@users.noreply.github.com>
2023-02-04 22:03:36 +0100

    Fix some bugs and polish some things
    Improved first level
    Fixed scale(rotation) Hero
    Fixed Collider friction

--- Assets/Script/CharacterMovement.cs
+++ Assets/Script/CharacterMovement.cs
@@ -7,7 +7,7 @@ public class CharacterMovement : MonoBehaviour
     private float horizontal;
     private bool m_FacingRight = true;
     private float distanceToGround = 0.2f;
-    private float distanceToCeiling = 0.5f;
+    private float distanceToCeiling = 0.2f;
 
     private int playerLayerInt, platformLayerInt;
 
@@ -31,7 +31,7 @@ public class CharacterMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
@@ -65,10 +65,12 @@ public class CharacterMovement : MonoBehaviour
 
     private void Flip()
     {
-        m_FacingRight = !m_FacingRight;
-
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        if(m_FacingRight && horizontal < 0f || !m_FacingRight && horizontal> 0f)
+        {
+            m_FacingRight = !m_FacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
